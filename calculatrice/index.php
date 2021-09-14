0ed649d47a054de5b6471f515e5dbf259fe021e4
Hissene Abdelwahid <teling39@yahoo.fr>
2021-09-14 14:10:05 +0100

    Add files via upload

---
+++ calculatrice/index.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require 'config/database.php';
+
+require_once 'views/.view.php';?>
---
+++ calculatrice/views/index.view.php
@@ -0,0 +1,5 @@
+<?php 
+require '_partials/_header.php'; 
+?>
+
+<?php require '_partials/_footer.php'; ?>
